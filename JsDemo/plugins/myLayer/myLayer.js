b42d1e30bd4ce3a5260510dd7111091db53e0247
wangx <824092231@qq.com>
2018-02-02 23:42:30 +0800

    弹出层插件编写 编写弹出层样式

---
+++ JsDemo/plugins/myLayer/myLayer.js
@@ -0,0 +1,35 @@
+﻿/**
+ * author:王新
+ *
+ * time:2018/02/02
+ *
+ * describe:采用闭包方式 实现弹出层插件
+ */
+
+var myLayer = (function(){
+
+	this._init = function(){
+		
+	}
+
+	return {
+		init:_init
+	}
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
