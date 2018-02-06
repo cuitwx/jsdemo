6ff69918f5a1e8d048b29567d71bc5bbd60cd48c
wangx <824092231@qq.com>
2018-02-06 23:21:37 +0800

    弹出层插件重构：使用了新的插件开发框架

--- JsDemo/plugins/myLayer/myLayer.js
+++ JsDemo/plugins/myLayer/myLayer.js
@@ -6,18 +6,55 @@
  * describe:采用闭包方式 实现弹出层插件
  */
 
-var myLayer = (function(){
+(function(){
 
-	this._init = function(){
-		
+	//首先定义默认参数
+	var options = {
+		width:"100px"
+		,height:"100px"
+		,title:"弹出层"		
 	}
 
-	return {
-		init:_init
+
+	//可定义其它方法
+
+
+	//定义api
+	var api = {
+		//配置方法：修改默认参数
+		//opts：新的参数
+		config:function(opts){
+			if(!opts) return options;
+
+			//遍历
+			for(var key in opts){
+				options[key] = opts[key];
+			}
+
+			return this;
+		}
+		//监听方法：监听dom元素
+		,listen:function(elems){
+
+			document.write('old options title：' + options.title + '<br>');
+
+			options.title = "新的弹出层";
+
+			document.write('new options title：' + options.title);
+
+		}
+		//初始化弹出层
+		,init:function(){
+			
+		}
 	}
 
-})();
 
+	//定义插件名字，将api结合起来
+	this.myLayer = api;
+
+
+})(); //匿名函数方式
 
 
 
